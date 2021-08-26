41093bee346e07e923a72090109dff9ba5ac18ee
William Walsh <wmdwalsh0@gmail.com>
2021-08-26 08:56:29 -0400

    completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
 
-end
+    def name=(name)
+      @name = name
+    end
+  
+    def name
+      @name
+    end
+
+    def breed=(breed)
+        @breed = breed
+      end
+    
+      def breed
+        @breed
+      end
+  end
+  
